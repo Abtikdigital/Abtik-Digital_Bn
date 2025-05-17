82ba9d8282e88c53ae9e353bdc9f70bec9468ab5
Abtikdigital <rbhensla1994@gmail.com>
2025-05-17 14:10:59 +0530

    changed url

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const emailMarketingRoutes = require("./routes/emailMarketingRoutes.js");
 
 const PORT = process.env.port;
 const app = express();
-console.log("this is frontend url ",process.env.front_end_url)
+
 app.use(
   cors({
-    origin: ["https://bejewelled-kashata-badc16.netlify.app"],
+    origin: [process.env.front_end_url],
     credentials: true,
   })
 );
